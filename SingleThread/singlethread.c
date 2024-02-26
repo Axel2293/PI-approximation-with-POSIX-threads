bee400a539d4d15b43547da04c9e87cccbb9e1a1
Axel Escoto Garcia <axel.escoto@iteso.mx>
2024-02-26 17:15:37 -0600

    Outputs formated and precision as argument

--- SingleThread/singlethread.c
+++ SingleThread/singlethread.c
@@ -1,23 +1,37 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-double rectangels=100000000;
-
-int main(void)
+int main(int argc, char **argv)
 {
-    double pi=0;
-    double base_all= (double)1/rectangels; // Devide 1 to get all the bases of the rectangel
-    double half=base_all/2.0;
 
-    for (double i = 0; i <= 1.0; i+=base_all)
+    if (argc != 2)
     {
-        double base_curr = i+base_all;
-        double height= sqrt(1.0-pow(i-half, 2)); //Calculate the height on the mid base point
-        pi+=base_all*height;
-
+        perror("[-] Too few Arguments\n\tUsage: piaproximation <Precision (num of rectangels)>");
+        exit(EXIT_FAILURE);
     }
-    printf("\nPI: %.16F\n", pi*4);
+    long precision = atol(*(argv + 1));
+    printf("[+] Precision set to %ld\n", precision);
+
+    double pi = 0;
+    double base_all = (double)1 / (double)precision; // Devide 1 to get all the bases of the rectangel
+    double half = base_all / 2.0;
+
+    struct timeval tval_before, tval_after, tval_result;
+    gettimeofday(&tval_before, NULL); // Take time at start of algorithm
+    // Start of calculations
+    for (double i = 0; i <= 1.0; i += base_all)
+    {
+        double base_curr = i + base_all;
+        double height = sqrt(1.0 - pow(i - half, 2)); // Calculate the height on the mid base point
+        pi += base_all * height;
+    }
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result); // Get execution time
+
+    printf("[+]\tFinished with time %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+    printf("[+]\tPI: %.20F\n", pi * 4);
 
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -6,73 +6,83 @@
 #include <sys/time.h>
 
 #define NTHREADS 4
-#define rectangels 100000000000
 
 pthread_mutex_t *lock;
- 	//Divides the calculation area for the threads
-double part =1.0/(double)NTHREADS;
-	// Calculates the bases for every rectangel
-double base = 1.0/ (double)rectangels;
-double  pi;
+double part;
+double base;
+double base_half;
+double pi;
 
-void * thread_func(void* arg){
+void *thread_func(void *arg)
+{
 
-	int *thread_num = (int*)arg;
-	printf("[+]\tThread %d; Starting calculations\n",*thread_num);
+	int *thread_num = (int *)arg;
+	printf("[+] Thread %d; Starting calculations\n", *thread_num);
 	// Calculate ranges for the thread
 	double START = (*thread_num) * part;
-	double END = ((*thread_num) +1) * part;
+	double END = ((*thread_num) + 1) * part;
 
-	// double base_all=1.0/rectangels;
-	double base_half=base/2.0;
-	double area=0;
+	double area = 0;
 
-	for (double i = START; i <= END; i+=base)
+	for (double i = START; i <= END; i += base)
 	{
-		double height = sqrt(1.0-pow(i-base_half, 2));
-		area+=base*height;
+		double height = sqrt(1.0 - pow(i - base_half, 2));
+		area += base * height;
 	}
-	
+
 	pthread_mutex_lock(lock);
-	pi+=area;
+	pi += area;
 	pthread_mutex_unlock(lock);
 	return NULL;
-} 
+}
 
-int main(int argc, char*argv[]){
-	//Divides the calculation area for the threads
-	part =1.0/( double)NTHREADS;
-	// Calculates the bases for every rectangel
-	base = 1.0/ ( double)rectangels;
-	//printf("PART: %.20F\nBASE: %.20F\n", part, base);
+int main(int argc, char **argv)
+{
 
-	pthread_t *tid= malloc(sizeof(pthread_t)*NTHREADS); //To store TID of threads
-	int *t_num=malloc(sizeof(int)*NTHREADS); // Numbers of threads
-	lock = malloc(sizeof(pthread_mutex_t)); // Mutex mem
-	pthread_mutex_init(lock,NULL); 	//Create mutex
+	if (argc != 2)
+	{
+		perror("[-] Too few Arguments\n\tUsage: piaproximation <Precision (num of rectangels)>");
+		exit(EXIT_FAILURE);
+	}
+	long precision = atol(*(argv + 1));
+	printf("[+] Precision set to %ld\n", precision);
+
+	// Divides the calculation area for the threads
+	part = 1.0 / (double)NTHREADS;
+	// Calculates the bases for every rectangle
+	base = 1.0 / (double)precision;
+	// Calculate the midpoint of every base
+	base_half = base / 2.0;
+
+	pthread_t *tid = malloc(sizeof(pthread_t) * NTHREADS); // To store TID of threads
+	int *t_num = malloc(sizeof(int) * NTHREADS);		   // Numbers of threads
+	lock = malloc(sizeof(pthread_mutex_t));				   // Mutex mem
+	pthread_mutex_init(lock, NULL);						   // Create mutex
 
 	struct timeval tval_before, tval_after, tval_result;
 	gettimeofday(&tval_before, NULL);
 
 	for (uint16_t i = 0; i < NTHREADS; i++)
 	{
-		t_num[i]=i;
+		t_num[i] = i;
 		// Create thread
-		pthread_create(&tid[i], NULL, thread_func, &t_num[i]);	
+		if (pthread_create(&tid[i], NULL, thread_func, &t_num[i]) != 0)
+		{
+			perror("[-] Could not create Thread");
+			exit(EXIT_FAILURE);
+		}
 	}
 
-	//Wait for threads to finish
+	// Wait for threads to finish
 	for (size_t i = 0; i < NTHREADS; i++)
 	{
-		//Wait for threads to finish
 		pthread_join(tid[i], NULL);
 	}
 	gettimeofday(&tval_after, NULL);
 	timersub(&tval_after, &tval_before, &tval_result);
 
 	printf("[+]\tThreads finished with time %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
-	printf("[+]\tPI result: %.16F\n", pi*(( double)4));
-	
-	
+	printf("[+]\tPI result: %.20F\n", pi * ((double)4));
+
 	return 0;
 }
